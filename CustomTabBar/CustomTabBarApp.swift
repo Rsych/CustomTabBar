493738d3dd9943990fcc534f6e4575daa0cb9f16
Ryan <tamielryan@gmail.com>
2021-12-12 15:55:15 +0900

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Ryan J. W. Kim on 2021/12/12.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
